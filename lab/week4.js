7d7d55a21fbedb3e54ac5129229aae68dc77ca70
meganmcevily <mgmcevily@gmail.com>
2021-01-29 14:55:04 -0600

    pre refactor for functions

--- lab/week4.js
+++ lab/week4.js
@@ -19,6 +19,40 @@ async function pageLoaded() {
   // writes the returned JSON to the console
   console.log(json)
   
+
+  // 1. get the data you're working with ✅
+  let products = json.products
+  console.log(products)
+
+
+  // 2. write a loop through the products ✅
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i]
+    // console.log(product)
+
+  // 3. render each product: make it work first ✅
+   let element = document.querySelector(`.products`)
+  //  console.log(element)
+
+  // 3a. render each product ✅ (div section taken from HTML file, edited hard coded details)
+   element.insertAdjacentHTML('beforeend', `
+        <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+          <div class="border h-full p-4 flex flex-col">
+           <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+          <div class="mb-4"><img src="${product.image}">
+          </div>
+          <div class="mb-4 text-gray-900">
+            ${product.description}
+          </div>
+          <div class="mt-auto text-purple-500 text-2xl">$${product.price}</div>
+          </div>
+        </div>
+   `)
+  
+  // 4. write code to loop through the products
+  // notes - you need to define the function outside of the loop (??)
+  }
+
   // 🔥 start here: write code to loop through the products; each
   // iteration of the loop should call your renderProduct function
   // make it work first; then extract to the separate renderProduct
